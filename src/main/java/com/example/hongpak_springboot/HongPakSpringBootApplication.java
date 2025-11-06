a9f293b4fda3057f47deb1e16cb5101b898d2cfb
sanggak2 <blizard0987@gmail.com>
2025-11-07 01:26:19 +0900

    feat : static 헬로월드 출력

---
+++ src/main/java/com/example/hongpak_springboot/HongPakSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.hongpak_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongPakSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongPakSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hongpak_springboot/HongPakSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hongpak_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongPakSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
