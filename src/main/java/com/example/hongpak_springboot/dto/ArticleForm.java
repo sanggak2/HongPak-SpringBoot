933f3043732434a2412185e3244b44b5e2b0a7df
sanggak2 <blizard0987@gmail.com>
2025-11-07 02:17:23 +0900

    feat : 폼 데이터 주고받기

---
+++ src/main/java/com/example/hongpak_springboot/dto/ArticleForm.java
@@ -0,0 +1,20 @@
+package com.example.hongpak_springboot.dto;
+
+//form데이터를 받아올 그릇
+public class ArticleForm {
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
