eb3dcebae91d6f115c1b909858658ff5b85fe40b
sanggak2 <blizard0987@gmail.com>
2025-11-07 22:37:46 +0900

    feat : DB에서 데이터 읽기

--- src/main/java/com/example/hongpak_springboot/repository/ArticleRepository.java
+++ src/main/java/com/example/hongpak_springboot/repository/ArticleRepository.java
@@ -3,7 +3,12 @@ package com.example.hongpak_springboot.repository;
 import com.example.hongpak_springboot.entity.Article;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
     // CrudRepository<Article, Long>이 SpringBoot에서 구현한 CRUD리포지토리.
     // 직접 구현 안해도됨
+
+    @Override
+    ArrayList<Article> findAll();
 }
