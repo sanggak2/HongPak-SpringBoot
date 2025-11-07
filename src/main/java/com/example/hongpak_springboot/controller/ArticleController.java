aa0e804267877f91a44e17832adf0fce4a405a3d
sanggak2 <blizard0987@gmail.com>
2025-11-07 16:09:22 +0900

    feat : H2 DB와 연결해서 폼 데이터 주고받기

--- src/main/java/com/example/hongpak_springboot/controller/ArticleController.java
+++ src/main/java/com/example/hongpak_springboot/controller/ArticleController.java
@@ -1,12 +1,19 @@
 package com.example.hongpak_springboot.controller;
 
 import com.example.hongpak_springboot.dto.ArticleForm;
+import com.example.hongpak_springboot.entity.Article;
+import com.example.hongpak_springboot.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    @Autowired  // springboot에서 싱글턴으로 구현한 객체에 연결
+    private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -15,6 +22,14 @@ public class ArticleController {
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
         System.out.println(form);
+
+        // 1. dto를 엔티티로 변환
+        Article article = form.toEntity();
+        System.out.println(article);
+
+        // 2. repository에게 엔티티를 db안에 넣어줘 시전.
+        Article saved = articleRepository.save(article);
+        System.out.println(saved);
         return "";
     }
 }
--- src/main/java/com/example/hongpak_springboot/dto/ArticleForm.java
+++ src/main/java/com/example/hongpak_springboot/dto/ArticleForm.java
@@ -1,5 +1,7 @@
 package com.example.hongpak_springboot.dto;
 
+import com.example.hongpak_springboot.entity.Article;
+
 //form데이터를 받아올 그릇
 public class ArticleForm {
     private String title;
@@ -16,5 +18,9 @@ public class ArticleForm {
                 "title='" + title + '\'' +
                 ", content='" + content + '\'' +
                 '}';
+    }
+
+    public Article toEntity(){
+        return new Article(null, title, content);
     }
 }
